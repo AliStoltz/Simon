8eef83e28efb8723becaa9249de03caa2331d0a2
AliStoltz <ali.mstoltz@gmail.com>
2019-10-01 13:46:45 -0700

    added css structure, began pseudo code

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
 // console.log('its working yo!');
+
+// User Story
+
+// - user inputs their name through a prompt.
+// - score, rounds, and high score are recorded in the header above the device.
+
+// - the console is displayed in the center of the page with four buttons (red, green, yellow, blue).
+// - buttons change opacity with a toggle when clicked. 
+
+// - the computer “Simon” lights up buttons in a random order.
+// -the user must reproduce that order by clicking the buttons.
+
+// -for each successful reproduction, a point is awarded to the player.
+// - each Round continues until mistake is made by the user. 
+
+
+// Nice-to have
+// - buttons produce a specific tone when clicked
+
+// step one
+    // create a button attached to the simon image 
+    //add a litener to the button which will trigger the console to start the game.
+
+
+// step two
+    // create a function that causes a toggle opacity on each color
+    //create a lister that logs the buttons clicked into an array.
+
+// step 3
+    //create an array for the computers "button sequence"
+    // each round it should loop through and add new combinations to the array
+
+//step 4
+    // create a function that compares the computers sequence to the players input array
+
+// step 5
+    // create fucntions that update the score, round and highscore. 
